29a1d270577728b9e09937f368a3d96114696eba
Tibor Zalabai <xzalabai@stuba.sk>
2018-09-24 19:35:38 +0200

    test passing

--- 02-cvicenie/wawjs-cvicenie02/test/array.js
+++ 02-cvicenie/wawjs-cvicenie02/test/array.js
@@ -21,29 +21,44 @@ describe("Array API exploration", function() {
     });
 
     it("TODO: indexOf(NaN) - indexOf with SameValueZero", function() {
-        let arr = [1, NaN, 2, 0, -0];
+        let arr = [1, NaN, 2, 0, -0, 'afoj'];
 
         assert(indexOf(arr, 1) === 0);
-
         assert(indexOf(arr, NaN) === 1);
         assert(indexOf(arr, 0) === 3);
         assert(indexOf(arr, -0) === 3);
+        assert(indexOf(arr,'afoj')=== 5);
 
         function indexOf(arr, what) {
-        	//TODO: 
+            for (let i = 0; i < arr.length; i++) {
+              if (typeof(what) === 'number' &&  'number' === typeof(arr[i]) && isNaN(what) && isNaN(arr[i])){
+                return i;
+              }
+              else if (arr[i] === what) {
+                  return i;
+              }
+            }
         }
     });
     it("TODO: indexOf(NaN) - indexOf with SameValue", function() {
-        let arr = [1, NaN, 2, 0, -0];
-       	
+        let arr = [1, NaN, 2, 0, -0, 'ahoj'];
+
        	assert(indexOf(arr, 1) === 0);
 
         assert(indexOf(arr, NaN) === 1);
         assert(indexOf(arr, 0) === 3);
         assert(indexOf(arr, -0) === 4); //!
+        assert(indexOf(arr, 'ahoj') === 5);
 
         function indexOf(arr, what) {
-            //TODO:
+          for (let i = 0; i < arr.length; i++) {
+            if (typeof(what) === 'number' &&  'number' === typeof(arr[i]) && isNaN(what) && isNaN(arr[i]))
+              return i;
+            else if (arr[i] == what && (1/what === 1/arr[i]) )
+              return i;
+            else if (typeof(what) === 'string' && typeof(arr[i]) === 'string' && what === arr[i])
+              return i;
+          }
         }
     });
 
